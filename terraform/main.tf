eb52eb0166a6cf4cbf95c55bee937f18a57dc944
Jenish Bhagat <Jenish48@gmail.com>
2026-03-24 03:25:35 +0530

    fix yaml indentation

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,5 @@ module "eks" {
 
   vpc_id = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
-
-
+  
 }
--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -2,23 +2,42 @@ module "eks" {
    source = "terraform-aws-modules/eks/aws"
    version = "~> 21.0"
 
-  name = var.cluster_name
- kubernetes_version = "1.29"
- 
+  name    = var.cluster_name
+  kubernetes_version = "1.29"
+  
 
+
+  endpoint_private_access = true
+  endpoint_public_access = true
 
   vpc_id     = var.vpc_id
   subnet_ids = var.subnet_ids
 
   enable_cluster_creator_admin_permissions = true
 
+  addons = {
+    coredns    = {
+            most_recent = true
+    }
+    kube-proxy = {
+            most_recent = true
+    }
+    vpc-cni    = {
+            most_recent = true
+    }
+  }
+
   eks_managed_node_groups = {
-    default = {
+     nodegroup1 = {
+
       desired_size = 2
       max_size     = 3
       min_size     = 1
 
       instance_types = ["t3.small"]
+      iam_role_additional_policies = {
+        AmazonEKS_CNI_Policy = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+      }
     }
   }
 }
