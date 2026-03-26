ba5ff36f2a59c88c50a8e6e6a62bc33910f628bb
Jenish Bhagat <Jenish48@gmail.com>
2026-03-26 17:22:12 +0530

    backend added and many more update

--- Automating-Deployment-of-a-Dockerized-ToDo-App-on-AWS-EKS-Using-GitOps-and-ArgoCD/appcode/src/App.js
+++ Automating-Deployment-of-a-Dockerized-ToDo-App-on-AWS-EKS-Using-GitOps-and-ArgoCD/appcode/src/App.js
@@ -1,4 +1,3 @@
-// App.js File
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Container from "react-bootstrap/Container";
@@ -8,150 +7,111 @@ import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import ListGroup from "react-bootstrap/ListGroup";
-  
+
+/* const API_URL = "http://localhost:5000"; // change later for k8s */
+const API_URL = "http://host.docker.internal:5000";
+
 class App extends Component {
-    constructor(props) {
-        super(props);
-  
-        // Setting up state
-        this.state = {
-            userInput: "",
-            list: [],
-        };
-    }
-  
-    // Set a user input value
-    updateInput(value) {
-        this.setState({
-            userInput: value,
-        });
-    }
-  
-    // Add item if user input in not empty
-    addItem() {
-        if (this.state.userInput !== "") {
-            const userInput = {
-                // Add a random id which is used to delete
-                id: Math.random(),
-  
-                // Add a user value to list
-                value: this.state.userInput,
-            };
-  
-            // Update list
-            const list = [...this.state.list];
-            list.push(userInput);
-  
-            // reset state
-            this.setState({
-                list,
-                userInput: "",
-            });
-        }
-    }
-  
-    // Function to delete item from list use id to delete
-    deleteItem(key) {
-        const list = [...this.state.list];
-  
-        // Filter values and leave value which we need to delete
-        const updateList = list.filter((item) => item.id !== key);
-  
-        // Update list in state
-        this.setState({
-            list: updateList,
-        });
-    }
-  
-    editItem = (index) => {
-      const todos = [...this.state.list];
-      const editedTodo = prompt('Edit the todo:');
-      if (editedTodo !== null && editedTodo.trim() !== '') {
-        let updatedTodos = [...todos]
-        updatedTodos[index].value= editedTodo
-        this.setState({
-          list: updatedTodos,
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userInput: "",
+      list: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchTodos();
+  }
+
+  fetchTodos() {
+    fetch(`${API_URL}/todo`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ list: data });
       });
-      }
+  }
+
+  updateInput(value) {
+    this.setState({
+      userInput: value,
+    });
+  }
+
+  addItem() {
+    if (this.state.userInput !== "") {
+      fetch(`${API_URL}/todo`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: Date.now().toString(),
+          task: this.state.userInput,
+        }),
+      }).then(() => {
+        this.fetchTodos();
+        this.setState({ userInput: "" });
+      });
     }
-  
-    render() {
-        return (
-            <Container>
-                <Row
-                    style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        fontSize: "3rem",
-                        fontWeight: "bolder",
-                    }}
+  }
+
+  deleteItem(id) {
+    fetch(`${API_URL}/todo/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      this.fetchTodos();
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row style={{ textAlign: "center", fontSize: "2rem", fontWeight: "bold" }}>
+          TODO APP (DevOps Project 🚀)
+        </Row>
+
+        <hr />
+
+        <Row>
+          <Col md={{ span: 5, offset: 4 }}>
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="Add todo..."
+                value={this.state.userInput}
+                onChange={(e) => this.updateInput(e.target.value)}
+              />
+              <Button variant="dark" onClick={() => this.addItem()}>
+                ADD
+              </Button>
+            </InputGroup>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={{ span: 5, offset: 4 }}>
+            <ListGroup>
+              {this.state.list.map((item) => (
+                <ListGroup.Item
+                  key={item.id}
+                  style={{ display: "flex", justifyContent: "space-between" }}
                 >
-                    10WeeksofCloudOps - TODO LIST
-                </Row>
-  
-                <hr />
-                <Row>
-                    <Col md={{ span: 5, offset: 4 }}>
-                        <InputGroup className="mb-3">
-                            <FormControl
-                                placeholder="add item . . . "
-                                size="lg"
-                                value={this.state.userInput}
-                                onChange={(item) =>
-                                    this.updateInput(item.target.value)
-                                }
-                                aria-label="add something"
-                                aria-describedby="basic-addon2"
-                            />
-                            <InputGroup>
-                                <Button
-                                    variant="dark"
-                                    className="mt-2"
-                                    onClick={() => this.addItem()}
-                                >
-                                    ADD
-                                </Button>
-                            </InputGroup>
-                        </InputGroup>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={{ span: 5, offset: 4 }}>
-                        <ListGroup>
-                            {/* map over and print items */}
-                            {this.state.list.map((item, index) => {
-                                return (
-                                  <div key = {index} > 
-                                    <ListGroup.Item
-                                        variant="dark"
-                                        action
-                                        style={{display:"flex",
-                                                justifyContent:'space-between'
-                                      }}
-                                    >
-                                        {item.value}
-                                        <span>
-                                        <Button style={{marginRight:"10px"}}
-                                        variant = "light"
-                                        onClick={() => this.deleteItem(item.id)}>
-                                          Delete
-                                        </Button>
-                                        <Button variant = "light"
-                                        onClick={() => this.editItem(index)}>
-                                          Edit
-                                        </Button>
-                                        </span>
-                                    </ListGroup.Item>
-                                  </div>
-                                );
-                            })}
-                        </ListGroup>
-                    </Col>
-                </Row>
-            </Container>
-        );
-    }
+                  {item.task}
+                  <Button
+                    variant="danger"
+                    onClick={() => this.deleteItem(item.id)}
+                  >
+                    Delete
+                  </Button>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
-  
+
 export default App;
--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,63 @@
 import express from "express";
 import AWS from "aws-sdk";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+const LOCAL_MODE = true; // 🔥 CHANGE THIS
+
+let todos = []; // local storage
 
 AWS.config.update({ region: "ap-south-1" });
-
 const dynamo = new AWS.DynamoDB.DocumentClient();
-const s3 = new AWS.S3();
 
-// 🔹 Save todo → DynamoDB
+// ✅ CREATE TODO
 app.post("/todo", async (req, res) => {
   const { id, task } = req.body;
 
+  if (LOCAL_MODE) {
+    todos.push({ id, task });
+    return res.json({ message: "Saved locally" });
+  }
+
   await dynamo.put({
     TableName: "todo-table",
-    Item: { id, task, status: "pending" }
+    Item: { id, task, status: "pending" },
   }).promise();
 
   res.send("Saved in DynamoDB");
 });
 
-// 🔹 Get todos
+// ✅ GET TODOS
 app.get("/todo", async (req, res) => {
+  if (LOCAL_MODE) {
+    return res.json(todos);
+  }
+
   const data = await dynamo.scan({
-    TableName: "todo-table"
+    TableName: "todo-table",
   }).promise();
 
   res.json(data.Items);
 });
 
-// 🔹 Upload file → S3
-app.get("/upload", async (req, res) => {
-  await s3.putObject({
-    Bucket: "jenish-todo-app-bucket",
-    Key: "demo.txt",
-    Body: "Hello from EKS"
+// ✅ DELETE TODO
+app.delete("/todo/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (LOCAL_MODE) {
+    todos = todos.filter((t) => t.id !== id);
+    return res.send("Deleted locally");
+  }
+
+  await dynamo.delete({
+    TableName: "todo-table",
+    Key: { id },
   }).promise();
 
-  res.send("Uploaded to S3");
+  res.send("Deleted from DynamoDB");
 });
 
-app.listen(3000, () => console.log("Backend running on port 3000"));
+app.listen(5000, () => console.log("Backend running on port 5000"));
